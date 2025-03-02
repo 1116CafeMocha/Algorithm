22aedbfaaeb63decee2b69d4133c7c56dd4b147b
1116CafeMocha <gpwmfsjt3@naver.com>
2025-03-02 20:08:45 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14624 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,35 @@
+// 백준 2293번. 동전 1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] input;
+
+		input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int k = Integer.parseInt(input[1]);
+
+		int[] coins = new int[n];
+		int[] dp = new int[k + 1];
+		dp[0] = 1;
+
+		for (int i = 0; i < n; i++) {
+			coins[i] = Integer.parseInt(br.readLine());
+		}
+
+		for (int coin : coins) {
+			for (int i = coin; i <= k; i++) {
+				dp[i] += dp[i - coin];
+			}
+		}
+
+		System.out.println(dp[k]);
+	}
+
+}
