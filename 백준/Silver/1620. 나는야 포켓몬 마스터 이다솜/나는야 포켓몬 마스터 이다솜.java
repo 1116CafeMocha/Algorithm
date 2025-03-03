8dd6a7e6f9547bcd4b31c8eb6473a5523608e5a7
1116CafeMocha <gpwmfsjt3@naver.com>
2025-03-03 19:22:48 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 888 ms, Memory: 46308 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,37 @@
+// 백준 1620번. 나는야 포켓몬 마스터 이다솜
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] input;
+		String str;
+		input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+
+		String[] array = new String[N + 1];
+		HashMap<String, Integer> map = new HashMap<>();
+
+		for (int i = 1; i <= N; i++) {
+			str = br.readLine();
+			array[i] = str;
+			map.put(str, i);
+		}
+
+		for (int i = 0; i < M; i++) {
+			str = br.readLine();
+			if (map.containsKey(str))
+				System.out.println(map.get(str));
+			else
+				System.out.println(array[Integer.parseInt(str)]);
+		}
+	}
+
+}
