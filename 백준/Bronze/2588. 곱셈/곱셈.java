dfb19ecf8fb59b67bc035d5e363e987545790c27
1116CafeMocha <gpwmfsjt3@naver.com>
2025-03-30 20:57:24 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+
+		bw.write(A * (B % 10) + "\n");
+		bw.write(A * (B % 100 / 10) + "\n");
+		bw.write(A * (B / 100) + "\n");
+		bw.write(A * B + "");
+
+		bw.flush();
+		bw.close();
+	}
+
+}
