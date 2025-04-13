7fd667fa3ffd0ac5f1d0f9adb71b39ffc97c0c17
1116CafeMocha <gpwmfsjt3@naver.com>
2025-04-13 23:14:24 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().split(" ");
+		int v = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		
+		for(int i=0; i<N; i++) {
+			int now = Integer.parseInt(input[i]);
+			if(now == v) count++;
+		}
+		
+		bw.write(count+"");
+		
+		bw.flush();
+		bw.close();
+	}
+}
