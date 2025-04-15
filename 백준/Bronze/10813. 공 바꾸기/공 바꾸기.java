e451bcc7396f85c35217971b94ba4c6ee98009cb
1116CafeMocha <gpwmfsjt3@naver.com>
2025-04-15 18:22:20 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+
+		int[] basket = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			basket[i] = i;
+		}
+
+		for (int i = 0; i < M; i++) {
+			input = br.readLine().split(" ");
+			int I = Integer.parseInt(input[0]);
+			int J = Integer.parseInt(input[1]);
+
+			int temp = basket[I];
+			basket[I] = basket[J];
+			basket[J] = temp;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			bw.write(basket[i] + " ");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
