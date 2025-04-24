3ca1d7d7f37be29c242a3503e9be23184c238b51
1116CafeMocha <gpwmfsjt3@naver.com>
2025-04-24 22:46:20 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i < N; i++) {
+			for (int k = 0; k < N - i; k++) {
+				bw.write(" ");
+			}
+			for (int k = 0; k < (i - 1) * 2 + 1; k++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+
+		for (int i = 0; i < 2 * (N - 1) + 1; i++) {
+			bw.write("*");
+		}
+
+		bw.write("\n");
+
+		for (int i = 1; i < N; i++) {
+			for (int k = 0; k < i; k++) {
+				bw.write(" ");
+			}
+			for (int k = 0; k < (N - i) * 2 - 1; k++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
