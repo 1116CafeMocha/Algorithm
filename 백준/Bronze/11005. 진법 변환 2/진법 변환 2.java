b1e627da6dc41b67ec0fbcf2601966fdb8252c3f
1116CafeMocha <gpwmfsjt3@naver.com>
2025-05-05 15:12:36 +0900

    [Bronze I] Title: 진법 변환 2, Time: 104 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] input = br.readLine().split(" ");
+		Long N = Long.parseLong(input[0]);
+		int B = Integer.parseInt(input[1]);
+		
+		bw.write(Long.toString(N, B).toUpperCase());
+
+		bw.flush();
+		bw.close();
+	}
+}
