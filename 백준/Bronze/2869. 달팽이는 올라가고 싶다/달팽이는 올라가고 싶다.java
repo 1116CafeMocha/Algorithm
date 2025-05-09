90bb8a0cda5f6a7dcf48abf8129ed57f6468a416
1116CafeMocha <gpwmfsjt3@naver.com>
2025-05-09 17:24:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] input = br.readLine().split(" ");
+		int A = Integer.parseInt(input[0]);
+		int B = Integer.parseInt(input[1]);
+		int V = Integer.parseInt(input[2]);
+
+		bw.write(Integer.toString((V - B - 1) / (A - B) + 1));
+
+		bw.flush();
+		bw.close();
+	}
+}
