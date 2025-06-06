c77a331377e7a58a702de4ca503baa4b917c0a40
1116CafeMocha <gpwmfsjt3@naver.com>
2025-06-06 20:13:38 +0900

    [Silver V] Title: 숫자 카드, Time: 1200 ms, Memory: 145472 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		String[] input = br.readLine().split(" ");
+
+		Set<Integer> set = new TreeSet<>();
+
+		for (int i = 0; i < N; i++) {
+			set.add(Integer.parseInt(input[i]));
+		}
+
+		int M = Integer.parseInt(br.readLine());
+
+		input = br.readLine().split(" ");
+
+		for (String now : input) {
+			if (set.contains(Integer.parseInt(now))) {
+				bw.write("1 ");
+			} else {
+				bw.write("0 ");
+			}
+		}
+
+		bw.flush();
+		bw.close();
+	}
+}
