1820ab6433bb79cccecbebe19c0409da603aa5c4
1116CafeMocha <gpwmfsjt3@naver.com>
2025-07-01 22:07:54 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -3,19 +3,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 		for(int i=0; i<100; i++) {
 			String input = br.readLine();
 			if(input == null) break;
-			System.out.println(input);
+			bw.write(input + "\n");
 		}
+
+		bw.flush();
+		bw.close();
 	}
 
 }
