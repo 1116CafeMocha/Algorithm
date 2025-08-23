7095333b01b86b8c4c5a06bad12635c6ec09e6b0
1116CafeMocha <gpwmfsjt3@naver.com>
2025-08-23 23:48:28 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			String[] input = br.readLine().split(" ");
+			int A = Integer.parseInt(input[0]);
+			int B = Integer.parseInt(input[1]);
+
+			System.out.println((A * A) / (B * B));
+		}
+	}
+}
