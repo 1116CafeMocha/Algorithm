b4dc672a9aa68d88a9808ab8bed9c7d1a5bea1cb
1116CafeMocha <gpwmfsjt3@naver.com>
2025-10-07 19:06:31 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,18 +9,20 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+
 		int L = Integer.parseInt(br.readLine());
 		char[] input = br.readLine().toCharArray();
-		
-		long val = 0;
-		
-		for(int i=0; i<L; i++) {
-			val += (input[i] - 'a' + 1) * ((long) Math.pow(31, i));
+
+		long val = 0L;
+		long p = 1L;
+
+		for (int i = 0; i < L; i++) {
+			val = (val + ((input[i] - 'a' + 1) * p) % 1234567891) % 1234567891;
+			p = (p * 31L) % 1234567891;
 		}
-		
-		bw.write(Long.toString(val % 1234567891));
-		
+
+		bw.write(Long.toString(val));
+
 		bw.flush();
 		bw.close();
 	}
