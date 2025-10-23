efa85cccf069baa2cb5b6013fce9d0f5ba4293aa
1116CafeMocha <gpwmfsjt3@naver.com>
2025-10-23 19:17:40 +0900

    [Silver III] Title: 등수 매기기, Time: 664 ms, Memory: 57476 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr);
+
+		long sum = 0;
+
+		for (int i = 0; i < N; i++) {
+			sum += Math.abs(arr[i] - (i + 1));
+		}
+
+		bw.write(Long.toString(sum));
+
+		bw.flush();
+		bw.close();
+	}
+
+}
