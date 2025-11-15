8efe00b6dfba9199c52d119a5f5d4c8b535cbf60
1116CafeMocha <gpwmfsjt3@naver.com>
2025-11-15 13:37:24 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 10.58 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,6 +1,11 @@
 class Solution {
     public String solution(String myString) {
-        String answer = myString.toUpperCase();
+        String answer = "";
+        char[] chars = myString.toCharArray();
+        
+        for(int i=0; i<chars.length; i++){
+            answer += Character.toUpperCase(chars[i]);
+        }
         
         return answer;
     }
