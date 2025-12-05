ef7fd04d0c44dea2cab5bcd8c889cfc9d37b8771
1116CafeMocha <gpwmfsjt3@naver.com>
2025-12-05 13:50:35 +0900

    [Silver III] Title: 수강신청, Time: 632 ms, Memory: 65660 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int K = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+
+		LinkedHashSet<String> set = new LinkedHashSet<>();
+		
+		for (int i = 0; i < L; i++) {
+			String now = br.readLine();
+			if (set.contains(now)) {
+				set.remove(now);
+				set.add(now);
+			} else {
+				set.add(now);
+			}
+		}
+
+		int idx = 0;
+		for (String now : set) {
+			if (idx >= K)
+				break;
+
+			bw.write(now);
+			bw.newLine();
+
+			idx++;
+		}
+
+		bw.flush();
+		bw.close();
+	}
+
+}
