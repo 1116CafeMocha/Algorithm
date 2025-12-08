3cf11788b30d0343f6954fefc023b7d2137a1948
1116CafeMocha <gpwmfsjt3@naver.com>
2025-12-09 00:28:26 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -6,53 +6,51 @@ import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] first = br.readLine().split(" ");
-		int N = Integer.parseInt(first[0]);
-		int M = Integer.parseInt(first[1]);
-
-		Map<String, Set<String>> groupToMembers = new HashMap<>();
-		Map<String, String> memberToGroup = new HashMap<>();
-
-		while (N-- > 0) {
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Map<String, Set<String>> groupToMem = new HashMap<>();
+		Map<String, String> memToGroup = new HashMap<>();
+		
+		while(N-- > 0) {
 			String group = br.readLine();
 			int num = Integer.parseInt(br.readLine());
-
-			Set<String> members = new TreeSet<>();
-
-			for (int i = 0; i < num; i++) {
+			for(int i=0; i<num; i++) {
 				String mem = br.readLine();
-				members.add(mem);
-				memberToGroup.put(mem, group);
-			}
-
-			groupToMembers.put(group, members);
-		}
-
-		while (M-- > 0) {
-			String name = br.readLine();
-			int type = Integer.parseInt(br.readLine());
-
-			if (type == 0) {
-				// 그룹 이름이 주어지면 -> 해당 그룹 멤버들 사전순 출력
-				for (String mem : groupToMembers.get(name)) {
-					bw.write(mem);
-					bw.newLine();
+				memToGroup.put(mem, group);
+				if(groupToMem.containsKey(group)) {
+					groupToMem.get(group).add(mem);
+				}else {
+					groupToMem.put(group, new TreeSet<>());
+					groupToMem.get(group).add(mem);
 				}
-			} else {
-				// 멤버 이름이 주어지면 -> 해당 멤버가 속한 그룹 이름 출력
-				bw.write(memberToGroup.get(name));
+			}
+		}
+		
+		while(M-- > 0) {
+			String qst = br.readLine();
+			int Q = Integer.parseInt(br.readLine());
+			if(Q == 0) {
+				for(String now : groupToMem.get(qst)) {
+					bw.write(now);
+					bw.newLine();
+				}				
+			}else {
+				bw.write(memToGroup.get(qst));
 				bw.newLine();
 			}
 		}
-
+		
 		bw.flush();
 		bw.close();
 	}
