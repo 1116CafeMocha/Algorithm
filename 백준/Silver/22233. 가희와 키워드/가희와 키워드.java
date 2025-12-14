e68f175f299b397e4f74be18d24da4b1441244ce
1116CafeMocha <gpwmfsjt3@naver.com>
2025-12-14 17:27:48 +0900

    [Silver III] Title: 가희와 키워드, Time: 1224 ms, Memory: 219768 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		Set<String> set = new HashSet<>();
+		while(N-- > 0) {
+			set.add(br.readLine());
+		}
+		while(M-- > 0) {
+			String[] input = br.readLine().split(",");
+			for(String now : input) set.remove(now);
+			bw.write(Integer.toString(set.size()));
+			bw.newLine();
+		}
+		
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
