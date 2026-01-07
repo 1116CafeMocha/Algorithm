ff92bb9c9a331c87a31801a5bc69b31872ab52f2
1116CafeMocha <gpwmfsjt3@naver.com>
2026-01-07 18:46:32 +0900

    [Silver V] Title: 이장님 초대, Time: 428 ms, Memory: 28004 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Integer[] arr = new Integer[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		int answer = 0;
+		for(int i=0; i<N; i++) {
+			int fin = arr[i] + i + 1;
+			if(fin > answer) answer = fin;
+		}
+		
+		answer++;
+		
+		bw.write(Integer.toString(answer));
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
