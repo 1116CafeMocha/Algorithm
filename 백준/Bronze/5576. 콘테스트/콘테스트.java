49b0c94bd41c940c85689d7a744e8e56863d484a
1116CafeMocha <gpwmfsjt3@naver.com>
2026-01-15 21:11:16 +0900

    [Bronze II] Title: 콘테스트, Time: 116 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Bronze/5576. 콘테스트/콘테스트.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[10];
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<10; j++) {
+				arr[j] = Integer.parseInt(br.readLine());
+			}
+			Arrays.sort(arr);
+			bw.write(arr[9]+arr[8]+arr[7] + " ");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
