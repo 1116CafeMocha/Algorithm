1f1c46e8ac3405506c48e5be566a96436b9ff979
1116CafeMocha <gpwmfsjt3@naver.com>
2026-01-21 21:15:57 +0900

    [Bronze IV] Title: 평균 점수, Time: 92 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int sum = 0;
+		for (int i = 0; i < 5; i++) {
+			int s = Integer.parseInt(br.readLine());
+			if(s < 40) s = 40;
+			sum+=s;
+		}
+		bw.write(Integer.toString(sum/5));
+		
+		bw.flush();
+		bw.close();
+	}
+}
