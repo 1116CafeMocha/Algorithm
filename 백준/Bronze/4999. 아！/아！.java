24ec3ebc799f88b81e1aa70bb13b9ed5ddd48207
1116CafeMocha <gpwmfsjt3@naver.com>
2026-02-04 16:30:58 +0900

    [Bronze V] Title: 아!, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str1 = br.readLine(); String str2 = br.readLine();
+		if(str1.length() < str2.length()) bw.write("no"); else bw.write("go");
+		
+		bw.flush();
+		bw.close();
+	}
+}
