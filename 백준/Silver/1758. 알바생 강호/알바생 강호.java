24ac85a1ab1b1eaef09d942492a8efb4cb8d4d76
1116CafeMocha <gpwmfsjt3@naver.com>
2026-03-03 00:47:12 +0900

    [Silver IV] Title: 알바생 강호, Time: 216 ms, Memory: 23152 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.java
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -1,30 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] tips = new int[N];
+		for(int i=0; i<N; i++) tips[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(tips);
+		
+		long sum = 0;
+		
+		for(int i=N-1; i>=0; i--) {
+			int tip = tips[i] - (N - i - 1);
+			if(tip <= 0) continue;
+			sum += tip;
+		}
+		
+		bw.write(Long.toString(sum));
+		
+		bw.flush();
+		bw.close();
+	}
 
-        int n = Integer.parseInt(br.readLine());
-        Integer[] tips = new Integer[n];
-
-        for (int i = 0; i < n; i++) {
-            tips[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(tips, Collections.reverseOrder());
-
-        long answer = 0;
-
-        for (int i = 0; i < n; i++) {
-            int realTip = tips[i] - i;
-            if (realTip <= 0) break;
-            answer += realTip;
-        }
-
-        System.out.println(answer);
-    }
 }
