b4c62afeab1478ded1c25b7ceb9e4d6478ec40fb
1116CafeMocha <gpwmfsjt3@naver.com>
2026-03-04 00:01:12 +0900

    [Silver V] Title: 스네이크버드, Time: 116 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,33 +1,38 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        
-        int[] fruits = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            fruits[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(fruits);
-        
-        for (int i = 0; i < N; i++) {
-            if (fruits[i] <= L) {
-                L++;
-            } else {
-                break;
-            }
-        }
-        
-        System.out.println(L);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		for(int i=0; i<N; i++) {
+			int now = arr[i];
+			if(now > L) break;
+			L ++;
+		}
+		
+		bw.write(Integer.toString(L));
+		
+		bw.flush();
+		bw.close();
+	}
+
 }
