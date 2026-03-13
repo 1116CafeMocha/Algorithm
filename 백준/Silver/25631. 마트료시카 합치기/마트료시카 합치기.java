b155092c0a08837daa5313af99fcd650f01e9e37
1116CafeMocha <gpwmfsjt3@naver.com>
2026-03-13 23:55:52 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 104 ms, Memory: 14592 KB -BaekjoonHub

---
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		
+		int answer = 1;
+        int count = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                answer = Math.max(answer, count);
+                count = 1;
+            }
+        }
+        
+        answer = Math.max(answer, count);
+        
+        bw.write(Integer.toString(answer));
+		
+		bw.flush();
+		bw.close();
+	}
+	
+}
