bfb580be4a196ac20fd60fc909f7074a297ca6cd
1116CafeMocha <gpwmfsjt3@naver.com>
2026-03-22 19:24:52 +0900

    [Silver IV] Title: 온라인 판매, Time: 128 ms, Memory: 16056 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[M];
+		
+		for(int i=0; i<M; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		int maxCost = 0;
+		int maxVal = 0;
+		
+		for(int i=M-1; i>=0; i--) {
+			if(Math.abs(i-M) > N) break;
+			int now = (Math.abs(i - M)) * arr[i];
+			if(now >= maxVal) {
+				maxCost = arr[i];
+				maxVal = now;
+			}
+		}
+		
+		bw.write(maxCost + " " + maxVal);
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
