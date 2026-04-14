ec542d355e5fe26c9cd1028109ec1f51acf89ded
1116CafeMocha <gpwmfsjt3@naver.com>
2026-04-14 23:58:59 +0900

    [Silver IV] Title: 수 고르기, Time: 140 ms, Memory: 15616 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/20186. 수 고르기/수 고르기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        long answer = 0;
+        int discount = 0;
+
+        for (int i = N - 1; i >= N - K; i--) {
+            answer += arr[i] - discount;
+            discount++;
+        }
+        
+        bw.write(Long.toString(answer));
+        
+        bw.flush();
+        bw.close();
+	}
+
+}
