9e63e95f6bd61b9721eee376623f02430e7591a7
1116CafeMocha <gpwmfsjt3@naver.com>
2026-04-17 20:32:42 +0900

    [level 2] Title: 가장 큰 수, Time: 2881.63 ms, Memory: 396 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        String[] arr = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++) arr[i] = Integer.toString(numbers[i]);
+        Arrays.sort(arr, (a, b) -> {
+            return (b+a).compareTo(a+b);
+        });
+        
+        if(Integer.parseInt(arr[0]) == 0) return "0";
+        
+        for(String now : arr) answer += now;
+        
+        return answer;
+    }
+}
