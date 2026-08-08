4de37cba72cbe9af6eec618d68d9fc18c66bf1f6
1116CafeMocha <gpwmfsjt3@naver.com>
2026-08-08 21:31:17 +0900

    [level 1] Title: 소수 찾기, Time: 26.19 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,14 +1,13 @@
 
 
 class Solution {
-    static int max = 1000000;
     
     public int solution(int n) {
         int answer = 0;
         
-        boolean[] sosu = new boolean[max + 1];
-        for(int i=2; i<=max; i++) {
-            for(int j=i+i; j<=max; j+=i) sosu[j] = true;
+        boolean[] sosu = new boolean[n + 1];
+        for(int i=2; i<=n; i++) {
+            for(int j=i+i; j<=n; j+=i) sosu[j] = true;
         }
         
         for(int i=2; i<=n; i++) if(!sosu[i]) answer++;
