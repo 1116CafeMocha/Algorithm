43fd5f5724dfa17c9b6db45fe9a3e837a595d7aa
1116CafeMocha <gpwmfsjt3@naver.com>
2026-08-28 21:26:05 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -2,10 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        List<Character> list = List.of('a','e','i','o','u');
-        char[] arr = my_string.toCharArray();
-        for(char now : arr) if(!list.contains(now)) sb.append(now);
-        return sb.toString();
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
