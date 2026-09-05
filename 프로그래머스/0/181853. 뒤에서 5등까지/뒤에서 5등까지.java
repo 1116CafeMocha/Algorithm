01017fab435b10980da67dba2f29e19f1993a6c2
1116CafeMocha <gpwmfsjt3@naver.com>
2026-09-05 21:41:08 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 1.69 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -3,6 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        return new int[]{num_list[0],num_list[1],num_list[2],num_list[3],num_list[4]};
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
