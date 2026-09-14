17b6700cbd4680f855697ce6ee5c5105ea625467
1116CafeMocha <gpwmfsjt3@naver.com>
2026-09-14 20:19:58 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        for(int i=1; i<=n; i+=2) answer[(i+1)/2-1] = i;
+        return answer;
+    }
+}
